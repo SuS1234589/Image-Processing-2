f2f6750d9d396f00378bbed8da7a335c33022ea7
ebm5630 <ebm5630@psu.edu>
2025-10-19 14:34:48 -0400

    finished 1 and 2, 2 need testing

---
+++ projection.m
@@ -0,0 +1,30 @@
+load('Parameters_V1_1.mat');
+load('mocapPoints3D.mat'); 
+
+% 3D world --> 3D camera
+R = Parameters.Rmat;
+C = Parameters.position;
+T = -R * C';
+N = size(pts3D, 2);
+p_cam_all = zeros(N, 3);
+
+for i = 1:N
+    p_world = pts3D(:, i);
+    p_cam_all(i, :) = (R * p_world + T)';
+end
+
+% 3D Camera --> 2D Film
+f = Parameters.foclen;
+X = p_cam_all(:, 1);
+Y = p_cam_all(:, 2);
+Z = p_cam_all(:, 3);
+x_film = f * (X ./ Z);
+y_film = f * (Y ./ Z);
+
+% 2D Film --> 2D Pixel 
+ox = Parameters.prinpoint(1);
+oy = Parameters.prinpoint(2);
+x = x_film + ox;
+y = y_film + oy;
+
+
